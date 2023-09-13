c863a52fca5ef35f5914d2d429381c2fee55c048
preawp <preawp@bu.edu>
2023-09-12 22:00:45 -0400

    done 3

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -1,17 +1,30 @@
 (* ****** ****** *)
 
 #use "./../assign0.ml";;
-
 (* ****** ****** *)
-
-let rec int2str(i0: int): string =
-    if i0 < 10
-        then str(chr(i0+48))
+(*recursive helper function to count digit of a number*)
+let rec int_len(i: int): int =
+    if i < 0 then
+    int_len(-i)
+    else if i < 10 then
+        1 
     else
-      let lastdigit = i0 mod 10 in
-      let remaining = i0 / 10 in
-      int2str remaining ^ str(chr(lastdigit + 48))
+        int_len(i / 10) + 1 
+    
+(*recursive helper function to extract individual digit*)
+let rec digit i n =
+    if i < 10 || n = 1 then
+        i mod 10
+    else
+        digit (i/10) (n-1)
 
+(*helper function to convert single digit to char*)
+let int2char(i: int): char =
+    chr(i + 48) 
+
+let int2str (i0: int) : string = 
+    let length = int_len i0 in
+    string_init (length) (fun j -> int2char(digit i0 (length - j )))
 (* ****** ****** *)
 
 (* end of [CS320-2023-Fall-assign0-3.ml] *)
