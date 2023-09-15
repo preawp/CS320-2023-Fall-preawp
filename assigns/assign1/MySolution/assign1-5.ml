9813b07972c1469761bbd1ea5f26a30ffeec30b6
preawp <preawp@bu.edu>
2023-09-15 13:16:32 -0400

    add instructions comments for each

---
+++ assigns/assign1/MySolution/assign1-5.ml
@@ -0,0 +1,23 @@
+
+(*
+assign1-5: 20 points
+
+A sequence of chars is ascending if any char in
+the sequence is less than or equal to the following
+one (when the following one does exist).
+Given a string cs, please implement a function
+that find the longest ascending subsequence of [cs].
+If there are more than one such sequences, the left
+most one should be returned.
+
+fun string_longest_ascend(xs: string): string
+
+For instance, given "1324561111", the function
+string_longest_ascend returns "13456"
+
+For instance, given "1234561111", the function
+string_longest_ascend returns "123456"
+
+For instance, given "1234511111", the function
+string_longest_ascend returns "111111".
+*)
