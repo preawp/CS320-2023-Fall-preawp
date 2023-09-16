d65e24c098606f527974dc104ddd4a7bd4e4d3f0
preawp <preawp@bu.edu>
2023-09-16 19:23:23 -0400

    working on 2

--- assigns/assign1/MySolution/assign1-2.ml
+++ assigns/assign1/MySolution/assign1-2.ml
@@ -14,15 +14,28 @@ For instance, if cs1 = "abcde" and cs2 = "1234", then
 string_merge(cs1)(cs2) equals "1234abcde"
 *)
 
+(*recursive helper function to find smallest char*)
+let rec find_min_char (cs: string) (min_char: char): char =
+  if cs = "" then min_char
+  else 
+    let current_char = string_get_at cs 0 in
+    let rest_chars = string_make_fwork(fun work -> string_foreach cs (fun c -> if c <> current_char then work c)) in
+    let new_min_char = 
+      if current_char < min_char then current_char
+      else min_char in
+      find_min_char rest_chars new_min_char 
 
-let string_merge (cs1: string) (cs2: string): string =
-  "55";;
+(*recursive helper function to find reorder string*)
+let rec string_reorder (cs: string) (acc: string): string =
+  if string_length cs = 0 then
+    acc
+  else
+    let min_char = find_min_char cs 'z' in
+    let rest_chars = string_make_fwork (fun work -> string_foreach cs (fun c -> if c <> min_char then work c)) in
+    let new_acc = string_snoc acc min_char in 
+    string_reorder rest_chars new_acc 
 
-let
-string_append
-(cs1:string)
-(cs2:string): string =
-string_make_fwork
-(fun work ->
- (string_foreach(cs1)(work); string_foreach(cs2)(work)));;
-
+(*main function that combine two string and call reorder function to return correct result*)
+let string_merge(cs1: string) (cs2: string): string =
+  let unordered_string = string_make_fwork(fun work -> string_foreach cs1 work; string_foreach cs2 work) in
+  string_reorder unordered_string "" 
