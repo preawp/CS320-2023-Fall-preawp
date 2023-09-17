65ce8e1e40354a51da06d613a87d125c5d325e36
preawp <preawp@bu.edu>
2023-09-17 14:37:49 -0400

    passed 4

--- assigns/assign1/MySolution/assign1-1.ml
+++ assigns/assign1/MySolution/assign1-1.ml
@@ -1,4 +1,4 @@
-#use "./../../classlib/OCaml/MyOCaml.ml";; 
+#use "./../../../classlib/OCaml/MyOCaml.ml";; 
 
 (*
 assign1-1: 10 points
--- assigns/assign1/MySolution/assign1-4.ml
+++ assigns/assign1/MySolution/assign1-4.ml
@@ -1,4 +1,5 @@
 #use "./../../../classlib/OCaml/MyOCaml.ml";; 
+
 (*
 assign1-4: 20 points
 Given two strings ds1 and ds2 of digits, please
@@ -17,21 +18,65 @@ Note that ds1 and ds2 can be arbitrarily long. Thus,
 converting ds1 and ds2 to integers can cause overflow.
 *)
 
-(* Function to add two strings representing non-negative integers digit by digit *)
-(* Function to add two strings representing non-negative integers digit by digit *)
-let intrep_add(ds1: string)(ds2: string): string =
-  let rec add_strings_with_carry ds1 ds2 carry result =
-    match (ds1, ds2) with
-    | ("", "") -> if carry > 0 then string_cons (char_of_digit carry) result else result
-    | _ ->
-        let digit1 = digit_of_char (char_of_string ds1.string_length ds1 - 1) in
-        let digit2 = digit_of_char (char_of_string ds2.string_length ds2 - 1) in
-        let (new_digit, new_carry) = add_digits digit1 digit2 carry in
-        let new_result = string_cons (char_of_digit new_digit) (result) in
-        add_strings_with_carry (string_sub ds1 0 (String.length ds1 - 1)) (string_sub ds2 0 (String.length ds2 - 1)) new_carry new_result
+(*function from lecture to reverse a string*)
+let
+stringrev(cs:string): string =
+string_make_fwork(string_rforeach(cs))
+
+(*function to choose max length of two strings*)
+let choose_max_len (str1: string)(str2: string): int =
+  let len1 = string_length str1 in
+  let len2 = string_length str2 in
+  if len1 >= len2 then len1
+  else len2
+
+(*function from lecture to convert int2str*)
+  let
+  int2str(n0: int): string =
+  let rec
+  foreach(n0: int)
+  (work: char -> unit) =
+  if
+  n0 >= 10
+  then
+  let
+  d0 =
+  char_of_digit(n0 mod 10) in
+  (work(d0); foreach(n0/10)(work))
+  else work(char_of_digit(n0))
+  in
+    string_rmake_fwork(foreach(n0))
+
+(*A string concatenation function*)
+let string_concat_local (str1:string) (str2:string): string = 
+    let string_list = [str1; str2] in
+    string_concat_list string_list
+
+let intrep_add (ds1: string) (ds2: string): string =
+
+  let num1 = stringrev ds1 in
+  let num2 = stringrev ds2 in
+  let len1 = string_length num1 in
+  let len2 = string_length num2 in
+
+  let max_len = choose_max_len ds1 ds2 in
+
+  let rec intrep_add_helper (i: int) (carry: int) (acc:string): string =
+    if i >= max_len then
+      if carry > 0 then
+        string_concat_local (int2str carry) acc
+      else
+        acc
+    else
+      let digit1 = if i < len1 then ord(string_get_at num1 i) - ord ('0') else 0 in
+      let digit2 = if i < len2 then ord(string_get_at num2 i) - ord('0') else 0 in
+      let sum = digit1 + digit2 + carry in
+      let new_carry = sum / 10 in
+      let new_digit = sum mod 10 in
+      intrep_add_helper (i + 1) new_carry (string_concat_local (int2str new_digit) acc)
   in
 
-  let result = add_strings_with_carry ds1 ds2 0 "" in
-  if result = "" then "0" else result
-;;
+  let result = intrep_add_helper 0 0 "" in
 
+  result
+;;
