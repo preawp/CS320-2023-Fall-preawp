61e41cb8ae7d941c4d446d74ffeab9f3f948e08c
preawp <preawp@bu.edu>
2023-09-19 15:49:57 -0400

    update 2

--- assigns/assign1/MySolution/assign1-2.ml
+++ assigns/assign1/MySolution/assign1-2.ml
@@ -13,40 +13,26 @@ string_merge(cs1)(cs2) equals "1234568"
 For instance, if cs1 = "abcde" and cs2 = "1234", then
 string_merge(cs1)(cs2) equals "1234abcde"
 *)
-
-(*recursive helper function to find smallest char*)
-
-let remove_char_once (cs: string) (char_to_remove: char): string =
-  let removed = ref false in
-  string_make_fwork (fun work ->
-    string_foreach cs (fun c ->
-      if c <> char_to_remove || !removed then
-        work c
-      else
-        removed := true
+let string_merge(cs1: string)(cs2: string): string =
+  let merged_string =
+    string_make_fwork (fun work ->
+      let len1 = string_length cs1 in
+      let len2 = string_length cs2 in
+      let rec merge_chars i1 i2 =
+        if i1 < len1 || i2 < len2 then (
+          let char1 = if i1 < len1 then string_get_at cs1 i1 else '\x7F' in
+          let char2 = if i2 < len2 then string_get_at cs2 i2 else '\x7F' in
+          if char1 <= char2 then (
+            work char1;
+            merge_chars (i1 + 1) i2
+          ) else (
+            work char2;
+            merge_chars i1 (i2 + 1)
+          )
+        )
+      in
+      merge_chars 0 0
     )
-  )
-let rec find_min_char (cs: string) (min_char: char): char =
-  if cs = "" then min_char
-  else 
-    let current_char = string_get_at cs 0 in
-    let rest_chars = string_make_fwork(fun work -> string_foreach cs (fun c -> if c <> current_char then work c)) in
-    let new_min_char = 
-      if current_char < min_char then current_char
-      else min_char in
-      find_min_char rest_chars new_min_char 
-
-(*recursive helper function to find reorder string*)
-let rec string_reorder (cs: string) (acc: string): string =
-  if string_length cs = 0 then
-    acc
-  else
-    let min_char = find_min_char cs 'z' in
-    let rest_chars = string_make_fwork (fun work -> string_foreach cs (fun c -> if c <> min_char then work c)) in
-    let new_acc = string_snoc acc min_char in 
-    string_reorder rest_chars new_acc 
-
-(*main function that combine two string and call reorder function to return correct result*)
-let string_merge(cs1: string) (cs2: string): string =
-  let unordered_string = string_make_fwork(fun work -> string_foreach cs1 work; string_foreach cs2 work) in
-  string_reorder unordered_string "" 
+  in
+  merged_string
+;;
---
+++ assigns/assign1/MySolution/quiz1_prep.ml
@@ -0,0 +1,4 @@
+#use "./../../../classlib/OCaml/MyOCaml.ml";; 
+
+let string_con_local(c0:char)(str:string):string = 
+  string_make_fwork (work(c0); fun work -> string_foreach(cs)(work))
