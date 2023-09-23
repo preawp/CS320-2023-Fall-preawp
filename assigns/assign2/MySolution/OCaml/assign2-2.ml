6a2fc71a1576a715a12df18aa11a9e285eda6c6e
preawp <preawp@bu.edu>
2023-09-23 00:43:08 -0400

    passed Ocaml 1-4

--- assigns/assign2/MySolution/OCaml/assign2-2.ml
+++ assigns/assign2/MySolution/OCaml/assign2-2.ml
@@ -18,6 +18,7 @@ mylist_get_at(xs: 'a mylist)(i0: int): 'a = ...
 *)
 
 (* ****** ****** *)
+(*return length of list based on my type mylist*)
 let rec mylist_length(xs: 'a mylist): int = 
   match xs with 
   | MyNil -> 0 
@@ -25,18 +26,32 @@ let rec mylist_length(xs: 'a mylist): int =
   | MySnoc(t, _) -> 1 + mylist_length t
   | MyReverse(t) -> mylist_length t
   | MyAppend2(t1, t2) -> mylist_length t1 + mylist_length t2
-  
-let rec mylist_get_at(xs: 'a mylist)(i0: int): 'a =
-  match xs, i0 with
-  | MyCons(x1, _), 0 -> x1
-  | MySnoc(_, x1), 0 -> x1
-  | MyCons(_, rest), _ -> mylist_get_at(rest)(i0 - 1)
-  | MySnoc(rest, _), _ -> mylist_get_at(rest)(i0 - 1)
-  | MyReverse(rlst), _ -> mylist_get_at(rlst)(i0)
-  | MyAppend2(lst1, lst2), _ ->
-    let len1 = mylist_length(lst1) in
-    if i0 < len1 then mylist_get_at(lst1)(i0)
-    else mylist_get_at(lst2)(i0 - len1)
-  | _ -> mylist_subscript_exn()
-;;
 
+(** Reverses a generic list**)
+let rec list_reverse_mylist (xs: 'a mylist): 'a mylist =
+  match xs with
+  | MyNil -> MyNil
+  | MyCons (x, t) -> MyCons (x, list_reverse_mylist t)
+  | MySnoc (t, x) -> MySnoc (list_reverse_mylist t, x)
+  | MyReverse l -> l 
+  | MyAppend2 (t1, t2) -> MyAppend2 (list_reverse_mylist t2, list_reverse_mylist t1)
+  
+  
+(*computes the element of a given mylist at a given position, otherwise exception*)
+let rec mylist_get_at (xs: 'a mylist) (i0: int): 'a =
+  match xs with
+  | MyNil -> mylist_subscript_exn ()
+  | MyCons (x, _) when i0 = 0 -> x
+  | MyCons (_, rest) when i0 > 0 -> mylist_get_at rest (i0 - 1)
+  | MySnoc (init, x) ->
+    let len_init = mylist_length init in
+    if i0 = len_init then x
+    else if i0 < len_init && i0 >= 0 then mylist_get_at init i0
+    else mylist_subscript_exn ()
+  | MyReverse l -> mylist_get_at (list_reverse_mylist l) i0
+  | MyAppend2 (l1, l2) ->
+    let len_l1 = mylist_length l1 in
+    if i0 < len_l1 && i0 >= 0 then mylist_get_at l1 i0
+    else if i0 >= len_l1 && i0 < len_l1 + mylist_length l2 then mylist_get_at l2 (i0 - len_l1)
+    else mylist_subscript_exn ()
+  | _ -> mylist_subscript_exn ()
--- assigns/assign2/MySolution/OCaml/assign2-3.ml
+++ assigns/assign2/MySolution/OCaml/assign2-3.ml
@@ -1,7 +1,6 @@
 #use "./../../../../classlib/OCaml/MyOCaml.ml";; 
 #use "./../../assign2.ml";;
 
-
 (*
 //
 Assign2-3: 10 points
@@ -13,54 +12,17 @@ foldleft_to_iforeach
 (foldleft: ('xs, 'x0, int) foldleft): ('xs, 'x0) iforeach = ...
 *)
 
-(* ****** ****** *)
-let rec
-list_foldleft
-(xs: 'a list)
-(r0: 'r0)(fopr: 'r0 -> 'a -> 'r0): 'r0 =
-match xs with
-| [] -> r0
-| (x1 :: xs) ->
-  list_foldleft(xs)(fopr(r0)(x1))(fopr)
-;;
-let rec mylist_length(xs: 'a mylist): int = 
-  match xs with 
-  | MyNil -> 0 
-  | MyCons(_, t) -> 1 + mylist_length t
-  | MySnoc(t, _) -> 1 + mylist_length t
-  | MyReverse(t) -> mylist_length t
-  | MyAppend2(t1, t2) -> mylist_length t1 + mylist_length t2
-  
-let foldleft_to_iforeach (foldleft: ('xs, 'x0, int) foldleft): ('xs, 'x0) iforeach =
-  fun xs work ->
-    let rec helper i r0 =
-      match xs with
-      | MyNil -> ()
-      | MyCons(x1, rest) ->
-        begin
-          work i x1;
-          helper (i + 1) (foldleft rest r0 (fun acc x -> acc))
-        end
-      | MySnoc(rest, x1) ->
-        begin
-          helper i (foldleft rest r0 (fun acc x -> acc));
-          work (i + 1) x1
-        end
-      | MyReverse(rlst) -> helper i (foldleft rlst r0 (fun acc x -> acc))
-      | MyAppend2(lst1, lst2) ->
-        let len1 = mylist_length(lst1) in
-        let r1 = foldleft lst1 r0 (fun acc x -> acc) in
-        let r2 = foldleft lst2 r0 (fun acc x -> acc) in
-        for i = 0 to len1 - 1 do
-          work i (mylist_get_at(lst1) i);
-        done;
-        for i = len1 to len1 + mylist_length(lst2) - 1 do
-          work i (mylist_get_at(lst2) (i - len1));
-        done;
+(* converts by applying the given function f to each element in the collection xs,
+    while ignoring the final acc*)
+let foldleft_to_iforeach (foldleft: ('xs, 'x0, int) foldleft): ('xs, 'x0) iforeach = 
+  fun xs f ->
+    let rec helper t acc =
+      match t with
+      | [] -> ()
+      | x :: xs' -> f x;        
+        let _ = foldleft xs' (acc + 1) (fun r0 x0 -> r0) in
+        helper xs' acc
     in
-    helper 0
-;;
+    helper xs 0  
 
-
-(* ****** ****** *)
 
