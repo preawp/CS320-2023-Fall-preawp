e1652b634bd42df962b265012c4f13bf81269110
preawp <preawp@bu.edu>
2023-10-11 22:58:05 -0400

    almost

--- assigns/assign4/MySolution/OCaml/assign4-3.ml
+++ assigns/assign4/MySolution/OCaml/assign4-3.ml
@@ -23,8 +23,24 @@ type 'a gtree =
 | GTnil 
 | GTcons of 'a * ('a gtree list)
 
+(*dept-first search implementation*)
 let rec gtree_streamize_dfs (xs: 'a gtree): 'a stream =
-
-    match xs with
-    | GTnil -> StrNil
-    | GTcons(x1, xs') -> StrCons(x1,  gtree_streamize_dfs xs')
+    let rec dfs_helper (node: 'a gtree list): 'a stream = fun() -> 
+      match node with
+      | [] -> StrNil
+      | hd :: tl -> 
+        match hd with 
+        | GTnil -> dfs_helper tl()
+        | GTcons(x1, x2) -> StrCons(x1, dfs_helper(list_append x2 tl)) 
+    in dfs_helper [xs]
+  
+(*breadth-first search implementation*)
+let rec gtree_streamize_bfs(xs: 'a gtree): 'a stream =
+  let rec bfs_helper (node: 'a gtree list): 'a stream = fun() -> 
+    match node with
+    | [] -> StrNil
+    | hd :: tl ->
+       match hd with 
+      | GTnil -> bfs_helper tl()
+      | GTcons(x1, x2) -> StrCons(x1, bfs_helper(list_append tl x2)) 
+  in bfs_helper [xs]
